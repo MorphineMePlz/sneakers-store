5c70d014704716dfd4cded4a8d45c2e27595a25b
unknown <aziz706797@gmail.com>
2023-08-28 22:42:12 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,136 @@
+function App() {
+  return (
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/images/logo.png" alt="Logotype" />
+          <div>
+            <h3 className="text-uppercase"> React Sneakers</h3>
+            <p className="opacity-5"> Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="d-flex align-center">
+          <li className="mr-30">
+            <img width={18} height={18} src="/images/cart.svg" alt="Корзина" />
+            <span> 1205 руб.</span>
+          </li>
+          <li>
+            <img
+              width={18}
+              height={18}
+              src="/images/logo-like.svg"
+              alt="Закладки"
+            />
+          </li>
+          <li>
+            <img
+              width={18}
+              height={18}
+              src="/images/logo-user.svg"
+              alt="Пользователь"
+            />
+          </li>
+        </ul>
+      </header>
+      <div className="content p-40">
+        <h1 className="mb-40">Все кроссовки!!</h1>
+        <div className="sneakers d-flex">
+          <div className="card">
+            <img
+              width={113}
+              height={112}
+              src="/images/sneakers/sn-1.jpg"
+              alt="sneakers"
+            />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img
+                  width={11}
+                  height={11}
+                  src="/images/sn-plus.svg"
+                  alt="plus"
+                />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={113}
+              height={112}
+              src="/images/sneakers/sn-2.jpg"
+              alt="sneakers"
+            />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img
+                  width={11}
+                  height={11}
+                  src="/images/sn-plus.svg"
+                  alt="plus"
+                />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={113}
+              height={112}
+              src="/images/sneakers/sn-3.jpg"
+              alt="sneakers"
+            />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img
+                  width={11}
+                  height={11}
+                  src="/images/sn-plus.svg"
+                  alt="plus"
+                />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={113}
+              height={112}
+              src="/images/sneakers/sn-4.jpg"
+              alt="sneakers"
+            />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img
+                  width={11}
+                  height={11}
+                  src="/images/sn-plus.svg"
+                  alt="plus"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
